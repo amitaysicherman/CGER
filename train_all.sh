87795fc72783abf720894454d96fb6b5b54c2264
amitay.s <amitay.sich@gmail.com>
2025-04-01 22:04:43 +0300

    init

--- train_all.sh
+++ train_all.sh
@@ -4,7 +4,7 @@
 #SBATCH --requeue
 #SBATCH -c 8
 #SBATCH --gres=gpu:L40:1
-#SBATCH --array=0-3
+#SBATCH --array=0-7
 
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 
@@ -12,9 +12,18 @@ split_index=$(($SLURM_ARRAY_TASK_ID))
 if [ $split_index -eq 0 ]; then
   python train.py --size "m"
 elif [ $split_index -eq 1 ]; then
-  python train.py --size "l" --dropout 0.05
-elif [ $split_index -eq 2 ]; then
   python train.py --size "l" --dropout 0.2
+elif [ $split_index -eq 2 ]; then
+  python train.py --size "l" --dropout 0.05
 elif [ $split_index -eq 3 ]; then
   python train.py --size "l" --dropout 0.3
+elif [ $split_index -eq 4 ]; then
+  python train.py --size "m" --dropout 0.0
+elif [ $split_index -eq 5 ]; then
+  python train.py --size "l" --dropout 0.4
+elif [ $split_index -eq 6 ]; then
+  python train.py --size "m" --dropout 0.2
+elif [ $split_index -eq 7 ]; then
+  python train.py --size "l" --dropout 0.0
 fi
+    
