5855b55a8489cfde8942f22b7c962d4c16c74a5d
amitay.s <amitay.sich@gmail.com>
2025-04-06 12:15:42 +0300

    init

--- prep_drug_bank.py
+++ prep_drug_bank.py
@@ -41,12 +41,12 @@ if __name__ == "__main__":
         if len(indexes) == 1:
             train_indexes = indexes
             test_indexes = []
-
-        test_count = max(int(len(indexes) * 0.1), 1)
-        train_count = len(indexes) - test_count
-        print(f"train: {train_count}, test: {test_count}")
-        train_indexes = random.sample(indexes, train_count)
-        test_indexes = list(set(indexes) - set(train_indexes))
+        else:
+            test_count = max(int(len(indexes) * 0.1), 1)
+            train_count = len(indexes) - test_count
+            print(f"train: {train_count}, test: {test_count}")
+            train_indexes = random.sample(indexes, train_count)
+            test_indexes = list(set(indexes) - set(train_indexes))
         for i in train_indexes:
             train_smiles.append(all_smiles[i])
             train_fasta.append(all_fasta[i])
