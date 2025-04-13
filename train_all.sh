ed9c4704bdd85d3d2847858a741a6203c8c6c84b
amitay.s <amitay.sich@gmail.com>
2025-04-13 22:35:44 +0300

    init

--- train_all.sh
+++ train_all.sh
@@ -4,27 +4,14 @@
 #SBATCH --requeue
 #SBATCH -c 8
 #SBATCH --gres=gpu:L40:1
-#SBATCH --array=0-36
+#SBATCH --array=0-2
 
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 
 split_index=$(($SLURM_ARRAY_TASK_ID))
 configs=(
-"python train.py --pooling 1 --bottleneck_dim 128 --size 'l' --dropout 0.0 --epochs 100"
-"python train.py --pooling 0 --bottleneck_dim 0 --size 'l' --dropout 0.0 --epochs 20"
-"python train.py --pooling 0 --bottleneck_dim 0 --size 'l' --dropout 0.2 --epochs 20"
-"python train.py --pooling 0 --bottleneck_dim 0 --size 'l' --dropout 0.4 --epochs 20"
-"python train.py --pooling 0 --bottleneck_dim 128 --size 'l' --dropout 0.0 --epochs 20"
-"python train.py --pooling 0 --bottleneck_dim 128 --size 'l' --dropout 0.2 --epochs 20"
-"python train.py --pooling 0 --bottleneck_dim 128 --size 'l' --dropout 0.4 --epochs 20"
-"python train.py --pooling 1 --bottleneck_dim 0 --size 'l' --dropout 0.0 --epochs 20"
-"python train.py --pooling 1 --bottleneck_dim 0 --size 'l' --dropout 0.2 --epochs 20"
-"python train.py --pooling 1 --bottleneck_dim 0 --size 'l' --dropout 0.4 --epochs 20"
-"python train.py --pooling 1 --bottleneck_dim 128 --size 'l' --dropout 0.0 --epochs 20"
-"python train.py --pooling 1 --bottleneck_dim 128 --size 'l' --dropout 0.2 --epochs 20"
-"python train.py --pooling 1 --bottleneck_dim 128 --size 'l' --dropout 0.4 --epochs 20"
-"python train.py --pooling 1 --bottleneck_dim 0 --size 'l' --dropout 0.0 --epochs 50 --gen_mol 1"
-"python train.py --pooling 1 --bottleneck_dim 128 --size 'l' --dropout 0.0 --epochs 50 --gen_mol 1"
+"python train.py --pooling 1 --bottleneck_dim 128 --size 'l' --dropout 0.0 --epochs 100 --cold_fasta 1"
+"python train.py --pooling 1 --bottleneck_dim 128 --size 'l' --dropout 0.0 --epochs 100 --cold_smiles 1 --gen_mol 1"
 )
 # Get the config for the current index
 config=${configs[$split_index]}
