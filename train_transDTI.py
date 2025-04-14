c4764f7d2901af42d8ced53f4bb8bcfae76c86a9
amitay.s <amitay.sich@gmail.com>
2025-04-14 19:48:46 +0300

    init

---
+++ train_transDTI.py
@@ -0,0 +1,201 @@
+import torch
+from transformers import Trainer, TrainingArguments
+from torch.utils.data import Dataset as TorchDataset
+from os.path import join as pjoin
+import numpy as np
+import glob
+from transformers import AutoModel
+from transformers import AutoTokenizer
+import torch.nn as nn
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+class TransDTI(torch.nn.Module):
+    def __init__(self):
+        super(TransDTI, self).__init__()
+
+        self.prot_branch = nn.Sequential(
+            nn.Linear(1280, 1280),
+            nn.BatchNorm1d(1280)  # batch_normalization: BatchNormalization
+        )
+
+        # Branch 2
+        self.mol_branch = nn.Sequential(
+            nn.Linear(768, 768),
+            nn.BatchNorm1d(768)
+        )
+
+        self.post_concat = nn.Sequential(
+            nn.Linear(1280 + 768, 1024),
+            nn.Dropout(),
+            nn.Linear(1024, 512),
+            nn.Linear(512, 2)
+        )
+
+    def forward(self, prot, mol):
+        prot = self.prot_branch(prot)  # Output shape: (None, 1280)
+        mol = self.mol_branch(mol)  # Output shape: (None, 768)
+        concatenated = torch.cat((prot, mol), dim=-1)
+        output = self.post_concat(concatenated)  # Final output shape: (None, 3)
+        return output
+
+
+class SrcTgtDataset(TorchDataset):
+    def __init__(self, src_texts, tgt_texts, labels, src_tokenizer, tgt_tokenizer, src_encoder, tgt_encoder,
+                 max_length=512):
+        self.src_texts = src_texts
+        self.tgt_texts = tgt_texts
+        self.max_length = max_length
+        self.src_tokenizer = src_tokenizer
+        self.tgt_tokenizer = tgt_tokenizer
+        self.src_encoder = src_encoder
+        self.src_memory = {}
+        self.tgt_encoder = tgt_encoder
+        self.tgt_memory = {}
+
+        self.labels = labels
+
+    def __len__(self):
+        return len(self.src_texts)
+
+    def __getitem__(self, idx):
+        src_text = self.src_texts[idx]
+        tgt_text = self.tgt_texts[idx]
+        if src_text not in self.src_memory:
+            src_tokens = self.src_tokenizer(
+                src_text, max_length=self.max_length, truncation=True, padding="max_length", return_tensors="pt"
+            )
+            src_tokens = {k: v.to(device) for k, v in src_tokens.items()}
+            src_encoder_outputs = self.src_encoder(**src_tokens)
+            src_encoder_outputs = src_encoder_outputs.last_hidden_state.mean(dim=1)
+            src_encoder_outputs = src_encoder_outputs.squeeze(0).detach().cpu()
+            self.src_memory[src_text] = src_encoder_outputs
+        if tgt_text not in self.tgt_memory:
+            tgt_tokens = self.tgt_tokenizer(
+                tgt_text, max_length=self.max_length, truncation=True, padding="max_length", return_tensors="pt"
+            )
+            tgt_tokens = {k: v.to(device) for k, v in tgt_tokens.items()}
+            tgt_encoder_outputs = self.tgt_encoder(**tgt_tokens)
+            tgt_encoder_outputs = tgt_encoder_outputs.pooler_output
+            tgt_encoder_outputs = tgt_encoder_outputs.detach().cpu()
+            self.tgt_memory[tgt_text] = tgt_encoder_outputs
+
+        return dict(
+            tgt_encoder_outputs=self.tgt_memory[tgt_text],
+            src_encoder_outputs=self.src_memory[src_text],
+            labels=self.labels[idx],
+        )
+
+
+def compute_metrics(eval_preds):
+    predictions, labels = eval_preds
+    predictions = predictions.argmax(-1)
+    predictions = predictions.flatten()
+    labels = labels.flatten()
+    acc = (predictions == labels).sum() / len(labels)
+    precision = (predictions * labels).sum() / predictions.sum()
+    recall = (predictions * labels).sum() / labels.sum()
+    f1 = 2 * precision * recall / (precision + recall)
+    auc = (predictions == labels).sum() / len(labels)
+    return {
+        "accuracy": acc,
+        "precision": precision,
+        "recall": recall,
+        "f1": f1,
+        "auc": auc
+    }
+
+
+def get_all_lines(split, base_dir):
+    with open(f"data/{base_dir}/{split}_enzyme.txt", "r") as f:
+        pos_protein = f.read().splitlines()
+    with open(f"data/{base_dir}/{split}_reaction.txt", "r") as f:
+        pos_molecule = f.read().splitlines()
+    with open(f"data/{base_dir}/{split}_enzyme_neg.txt", "r") as f:
+        neg_protein = f.read().splitlines()
+    with open(f"data/{base_dir}/{split}_reaction_neg.txt", "r") as f:
+        neg_molecule = f.read().splitlines()
+    labels = [1] * len(pos_protein) + [0] * len(neg_protein)
+    prot_text = pos_protein + neg_protein
+    mol_texts = pos_molecule + neg_molecule
+    return prot_text, mol_texts, labels
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--base_data", type=str, default="drugbank", help="base data")
+
+    args = parser.parse_args()
+
+    prot_train, mol_train, labels_train = get_all_lines("train", args.base_data)
+    prot_valid, mol_valid, labels_valid = get_all_lines("valid", args.base_data)
+    prot_test, mol_test, labels_test = get_all_lines("test", args.base_data)
+
+    prot_tokenizer = AutoTokenizer.from_pretrained("facebook/esm2_t33_650M_UR50D")
+    prot_model = AutoModel.from_pretrained("facebook/esm2_t33_650M_UR50D")
+    prot_model = prot_model.to(device).eval()
+    for param in prot_model.parameters():
+        param.requires_grad = False
+    mol_tokenizer = AutoTokenizer.from_pretrained("ibm/MoLFormer-XL-both-10pct", trust_remote_code=True)
+    mol_model = AutoModel.from_pretrained("ibm/MoLFormer-XL-both-10pct", trust_remote_code=True,
+                                          deterministic_eval=True)
+    mol_model = mol_model.to(device).eval()
+    for param in mol_model.parameters():
+        param.requires_grad = False
+
+    train_dataset = SrcTgtDataset(prot_train, mol_train, labels_train, prot_tokenizer, mol_tokenizer,
+                                  prot_model, mol_model)
+    valid_dataset = SrcTgtDataset(prot_valid, mol_valid, labels_valid, prot_tokenizer, mol_tokenizer,
+                                  prot_model, mol_model)
+    test_dataset = SrcTgtDataset(prot_test, mol_test, labels_test, prot_tokenizer, mol_tokenizer,
+                                 prot_model, mol_model)
+
+    train_small_indices = np.random.choice(len(train_dataset), len(test_dataset), replace=False)
+    train_small_dataset = torch.utils.data.Subset(train_dataset, train_small_indices)
+
+    eval_dataset = {"test": test_dataset, "train": train_small_dataset, "valid": valid_dataset},
+    metric_for_best_model = "eval_valid_auc"
+
+    model = TransDTI()
+
+    output_dir = f"results_db/transDTI_{args.base_data}"
+    logs_dir = output_dir.replace("results", "logs")
+    training_args = TrainingArguments(
+        output_dir=output_dir,
+        logging_dir=logs_dir,
+        evaluation_strategy="steps",
+        learning_rate=1e-4,
+        per_device_train_batch_size=64,
+        per_device_eval_batch_size=64,
+        eval_accumulation_steps=30,
+        save_total_limit=3,
+        num_train_epochs=100,
+        logging_steps=100,
+        eval_steps=250,
+        save_strategy="steps",
+        save_steps=250,
+        lr_scheduler_type="constant",
+        load_best_model_at_end=True,
+        metric_for_best_model=metric_for_best_model,
+        report_to="tensorboard",
+        save_safetensors=False,
+        auto_find_batch_size=True,
+    )
+
+    # Define trainer
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset,
+        eval_dataset=eval_dataset,
+        compute_metrics=compute_metrics,
+    )
+    # Train model
+    print("Training model...")
+
+    trainer.train(resume_from_checkpoint=len(glob.glob(pjoin(output_dir, "checkpoint-*"))) > 0)
+
+    print("Training complete!")
