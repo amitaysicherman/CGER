e577d945b00ce813d4bb79600a1a64117b57331b
amitay.s <amitay.sich@gmail.com>
2025-04-14 09:07:49 +0300

    init

--- train_all.sh
+++ train_all.sh
@@ -10,8 +10,8 @@ export PYTHONPATH=$PYTHONPATH:$(pwd)
 
 split_index=$(($SLURM_ARRAY_TASK_ID))
 configs=(
-"python train.py --pooling 1 --bottleneck_dim 128 --size 'l' --dropout 0.0 --epochs 100 --cold_fasta 1"
-"python train.py --pooling 1 --bottleneck_dim 128 --size 'l' --dropout 0.0 --epochs 100 --cold_smiles 1 --gen_mol 1"
+"python train.py --pooling 1 --bottleneck_dim 128 --size 'l' --dropout 0.0 --epochs 100 --cold_fasta 1 --gen_mol 1"
+"python train.py --pooling 1 --bottleneck_dim 128 --size 'l' --dropout 0.0 --epochs 100 --cold_smiles 1"
 )
 # Get the config for the current index
 config=${configs[$split_index]}
