c7666743556917fa8b216e9885fab152a567c728
amitay.s <amitay.sich@gmail.com>
2025-04-15 23:00:47 +0300

    init

--- train.py
+++ train.py
@@ -370,7 +370,7 @@ if __name__ == "__main__":
     if args.cold_fasta:
         output_dir += "_cf"
     if args.random_tgt:
-        output_dir += "_rnd"
+        output_dir += f"_rnd{args.random_tgt}"
     if args.level == "drugbank":
         output_dir = output_dir.replace("results", "results_db")
     logs_dir = output_dir.replace("results", "logs")
