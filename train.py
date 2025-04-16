2e14c08632856fbd89c7c038622a6eec324f7e15
amitay.s <amitay.sich@gmail.com>
2025-04-16 08:58:54 +0300

    init

--- train.py
+++ train.py
@@ -278,7 +278,6 @@ class EndToEndModel(torch.nn.Module):
                 encoder_hidden_states = encoder_outputs.pooler_output.unsqueeze(1)
             else:
                 encoder_hidden_states = encoder_outputs.last_hidden_state.mean(dim=1)
-            encoder_hidden_states = encoder_hidden_states.unsqueeze(1)  # Add sequence dimension [batch, 1, hidden_dim]
             encoder_attention_mask = torch.ones(src_attention_mask.shape[0], 1).to(src_attention_mask.device)
         else:
             encoder_hidden_states = encoder_outputs.last_hidden_state
