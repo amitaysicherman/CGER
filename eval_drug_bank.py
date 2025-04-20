d92b418078206549bd8631ff0fe80a3cb533f359
amitay.s <amitay.sich@gmail.com>
2025-04-20 11:15:09 +0300

    init

--- eval_drug_bank.py
+++ eval_drug_bank.py
@@ -12,19 +12,20 @@ from sklearn.metrics import f1_score
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 from sklearn.metrics import roc_auc_score, accuracy_score
 
-def load_negative_files(split, mol,cold_smiles=0, cold_fasta=0, random_replace=None,quantize=False):
+
+def load_negative_files(split, mol, cold_smiles=0, cold_fasta=0, random_replace=None, quantize=False):
     basd_path = f"data/drugbank"
     if cold_smiles:
         basd_path += f"_cs"
     if cold_fasta:
         basd_path += f"_cf"
-    src_file =f"{basd_path}/{split}_reaction_neg.txt"
+    src_file = f"{basd_path}/{split}_reaction_neg.txt"
     tgt_file = f"{basd_path}/{split}_enzyme_neg.txt"
     if quantize:
         if mol:
-            src_file= src_file.replace(".txt", "_q.txt")
+            src_file = src_file.replace(".txt", "_q.txt")
         else:
-            tgt_file= tgt_file.replace(".txt", "_q.txt")
+            tgt_file = tgt_file.replace(".txt", "_q.txt")
     with open(src_file, "r") as f:
         src = f.read().splitlines()
     with open(tgt_file, "r") as f:
@@ -36,32 +37,39 @@ def load_negative_files(split, mol,cold_smiles=0, cold_fasta=0, random_replace=N
     return src, tgt
 
 
-def get_data(pooling, src_model, src_tokenizer, tgt_tokenizer, gen_mol, return_files=False,cold_smiles=0, cold_fasta=0,
-             random_replace=None,train_encoder=False,quantize=False):
+def get_data(pooling, src_model, src_tokenizer, tgt_tokenizer, gen_mol, return_files=False, cold_smiles=0, cold_fasta=0,
+             random_replace=None, train_encoder=False, quantize=False):
     src_train, tgt_train, src_valid, tgt_valid, src_test, tgt_test = load_files(level="drugbank", gen_mol=gen_mol,
-                                                                                     cold_smiles=cold_smiles,
-                                                                                     cold_fasta=cold_fasta,
-                                                                                     quantize=quantize)
+                                                                                cold_smiles=cold_smiles,
+                                                                                cold_fasta=cold_fasta,
+                                                                                quantize=quantize)
     if random_replace is not None:
         tgt_train = [random_replace.get_random_tokens(x) for x in tgt_train]
         tgt_valid = [random_replace.get_random_tokens(x) for x in tgt_valid]
         tgt_test = [random_replace.get_random_tokens(x) for x in tgt_test]
 
     if train_encoder:
-        pos_valid = SrcTgtDataset(src_valid, tgt_valid, src_tokenizer, tgt_tokenizer, None, pooling=pooling,train_encoder=True)
-        pos_test = SrcTgtDataset(src_test, tgt_test, src_tokenizer, tgt_tokenizer, None, pooling=pooling,train_encoder=True)
+        pos_valid = SrcTgtDataset(src_valid, tgt_valid, src_tokenizer, tgt_tokenizer, None, pooling=pooling,
+                                  train_encoder=True)
+        pos_test = SrcTgtDataset(src_test, tgt_test, src_tokenizer, tgt_tokenizer, None, pooling=pooling,
+                                 train_encoder=True)
     else:
         pos_valid = SrcTgtDataset(src_valid, tgt_valid, src_tokenizer, tgt_tokenizer, src_model, pooling=pooling)
         pos_test = SrcTgtDataset(src_test, tgt_test, src_tokenizer, tgt_tokenizer, src_model, pooling=pooling)
 
-    src_neg_valid, tgt_neg_valid = load_negative_files("valid", gen_mol,cold_smiles=cold_smiles, cold_fasta=cold_fasta,random_replace=random_replace,quantize=quantize)
+    src_neg_valid, tgt_neg_valid = load_negative_files("valid", gen_mol, cold_smiles=cold_smiles, cold_fasta=cold_fasta,
+                                                       random_replace=random_replace, quantize=quantize)
 
-    src_neg_test, tgt_neg_test = load_negative_files("test", gen_mol,cold_smiles=cold_smiles, cold_fasta=cold_fasta,random_replace=random_replace,quantize=quantize)
+    src_neg_test, tgt_neg_test = load_negative_files("test", gen_mol, cold_smiles=cold_smiles, cold_fasta=cold_fasta,
+                                                     random_replace=random_replace, quantize=quantize)
     if train_encoder:
-        neg_valid = SrcTgtDataset(src_neg_valid, tgt_neg_valid, src_tokenizer, tgt_tokenizer, None, pooling=pooling,train_encoder=True)
-        neg_test = SrcTgtDataset(src_neg_test, tgt_neg_test, src_tokenizer, tgt_tokenizer, None, pooling=pooling,train_encoder=True)
+        neg_valid = SrcTgtDataset(src_neg_valid, tgt_neg_valid, src_tokenizer, tgt_tokenizer, None, pooling=pooling,
+                                  train_encoder=True)
+        neg_test = SrcTgtDataset(src_neg_test, tgt_neg_test, src_tokenizer, tgt_tokenizer, None, pooling=pooling,
+                                 train_encoder=True)
     else:
-        neg_valid = SrcTgtDataset(src_neg_valid, tgt_neg_valid, src_tokenizer, tgt_tokenizer, src_model, pooling=pooling)
+        neg_valid = SrcTgtDataset(src_neg_valid, tgt_neg_valid, src_tokenizer, tgt_tokenizer, src_model,
+                                  pooling=pooling)
         neg_test = SrcTgtDataset(src_neg_test, tgt_neg_test, src_tokenizer, tgt_tokenizer, src_model, pooling=pooling)
     if return_files:
         return pos_valid, neg_valid, pos_test, neg_test, tgt_train
@@ -118,7 +126,6 @@ def evaluate_model(pos_dataset, neg_dataset, model, batch_size=32, return_prob=F
     neg_dataloader = DataLoader(neg_dataset, batch_size=batch_size, shuffle=False)
     print(f"Number of positive examples: {len(pos_dataset)}")
     print(f"Number of negative examples: {len(neg_dataset)}")
-
 
     pos_prob = []
 
@@ -178,14 +185,15 @@ class Config:
     mol: bool = True
     cold_smiles: int = 0
     cold_fasta: int = 0
+    quantize: bool = False
 
     def to_list(self):
-        return [self.size, self.dropout, self.pooling, self.bottleneck_dim, self.learning_rate, self.mol, self.cold_smiles,
-                self.cold_fasta]
+        return [self.size, self.dropout, self.pooling, self.bottleneck_dim, self.learning_rate, self.mol,
+                self.cold_smiles,
+                self.cold_fasta, self.quantize]
 
 
-
-def get_model(decoder,trie,size, dropout, pooling, bottleneck_dim, learning_rate, mol):
+def get_model(decoder, trie, size, dropout, pooling, bottleneck_dim, learning_rate, mol,quantize=False):
     encoder_dim = 768 if not mol else 1280
     model = EnzymeDecoder(decoder, trie=trie, encoder_dim=encoder_dim, bottleneck_dim=bottleneck_dim)
     n_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
@@ -198,7 +206,8 @@ def get_model(decoder,trie,size, dropout, pooling, bottleneck_dim, learning_rate
         output_dir += "_pooling"
     if mol:
         output_dir += "_mol"
-
+    if quantize:
+        output_dir += "_quantize"
     model_path = f"results_db/{output_dir}/"
     model_path = get_best_cp(model_path)
     model.load_state_dict(torch.load(model_path, map_location=device))
@@ -206,9 +215,10 @@ def get_model(decoder,trie,size, dropout, pooling, bottleneck_dim, learning_rate
     print(f"Model loaded from {model_path}")
     return model
 
+
 def main():
-    cong1 = Config("l", 0.0, True, 128, 0.0001, True, 0, 0)
-    cong2 = Config("l", 0.0, True, 128, 0.0001, False, 0, 0)
+    cong1 = Config("l", 0.0, True, 128, 0.0001, True, 0, 0, quantize=True)
+    cong2 = Config("l", 0.0, True, 128, 0.0001, False, 0, 0, quantize=True)
 
     all_scores = []
     all_true = []
@@ -216,41 +226,42 @@ def main():
     all_scores_val = []
     all_true_val = []
     for cong in [cong1, cong2]:
-        size, dropout, pooling, bottleneck_dim, learning_rate, mol, cold_smiles, cold_fasta = cong.to_list()
+        size, dropout, pooling, bottleneck_dim, learning_rate, mol, cold_smiles, cold_fasta, quantize = cong.to_list()
         reaction_model, reaction_tokenizer, decoder, esm_tokenizer = get_encoder_decoder(decoder_size=size,
                                                                                          dropout=dropout,
-                                                                                         drugbank=True, gen_mol=mol)
+                                                                                         drugbank=True, gen_mol=mol,
+                                                                                         )
 
         pos_valid, neg_valid, pos_test, neg_test, trie_files = get_data(pooling, reaction_model, reaction_tokenizer,
                                                                         esm_tokenizer,
                                                                         gen_mol=mol,
                                                                         return_files=True,
                                                                         cold_smiles=cold_smiles,
-                                                                        cold_fasta=cold_fasta,)
+                                                                        cold_fasta=cold_fasta, quantize=quantize)
+
+        if quantize:
+            from train import QuantizeTokenizer
+            esm_tokenizer = QuantizeTokenizer()
 
         trie = build_trie(list(set(trie_files)), esm_tokenizer, max_length=512)
         reaction_model.to(device).eval()
         decoder.to(device).eval()
-        model = get_model(decoder, trie, size, dropout, pooling, bottleneck_dim, learning_rate, mol)
-
+        model = get_model(decoder, trie, size, dropout, pooling, bottleneck_dim, learning_rate, mol,quantize=quantize)
 
         y_true, y_scores = evaluate_model(pos_test, neg_test, model, batch_size=32, return_prob=True)
         all_scores.append(y_scores)
         all_true.append(y_true)
 
-
         y_true_val, y_scores_val = evaluate_model(pos_valid, neg_valid, model, batch_size=32, return_prob=True)
         all_scores_val.append(y_scores_val)
         all_true_val.append(y_true_val)
 
-
-
-    y_true=all_true[0]
-    y_scores=all_scores[0]
-    y_true_val=all_true_val[0]
-    y_scores_val=all_scores_val[0]
+    y_true = all_true[0]
+    y_scores = all_scores[0]
+    y_true_val = all_true_val[0]
+    y_scores_val = all_scores_val[0]
     # Calculate metrics
-    auc= roc_auc_score(y_true, y_scores)
+    auc = roc_auc_score(y_true, y_scores)
     best_acc_threshold, best_acc_score = find_optimal_threshold(y_true_val, y_scores_val, metric='accuracy')
     best_f1_threshold, best_f1_score = find_optimal_threshold(y_true_val, y_scores_val, metric='f1')
     print(f"AUC: {auc:.4f}")
@@ -262,21 +273,18 @@ def main():
     print(f"Test accuracy score: {best_acc_score:.4f}")
     print(f"Test F1 score: {best_f1_score:.4f}")
 
-
     import matplotlib.pyplot as plt
     import seaborn as sns
     sns.set(style="whitegrid")
     plt.figure(figsize=(10, 6))
-    plt.hist(y_scores[y_true==1], bins=50, alpha=0.5, label='Positive', color='blue')
-    plt.hist(y_scores[y_true==0], bins=50, alpha=0.5, label='Negative', color='red')
+    plt.hist(y_scores[y_true == 1], bins=50, alpha=0.5, label='Positive', color='blue')
+    plt.hist(y_scores[y_true == 0], bins=50, alpha=0.5, label='Negative', color='red')
     plt.axvline(x=best_acc_threshold, color='green', linestyle='--', label='Best Accuracy Threshold')
     plt.axvline(x=best_f1_threshold, color='orange', linestyle='--', label='Best F1 Threshold')
     plt.xlabel('Probability')
     plt.ylabel('Frequency')
     plt.title('Histogram of Probabilities')
     plt.legend()
-
-
 
     assert (all_true[0] == all_true[1]).all()
     all_scores = np.stack(all_scores)
@@ -288,7 +296,6 @@ def main():
     print(f"Best accuracy threshold: {best_acc_threshold:.4f}, Best accuracy score: {best_acc_score:.4f}")
     print(f"Best F1 threshold: {best_f1_threshold:.4f}, Best F1 score: {best_f1_score:.4f}")
     # Save the results
-
 
 
 if __name__ == "__main__":
