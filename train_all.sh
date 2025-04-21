4f66af0720b2fd2e6781cc3b4fc041aa9a170b14
amitay.s <amitay.sich@gmail.com>
2025-04-21 16:22:22 +0300

    s

--- train_all.sh
+++ train_all.sh
@@ -11,14 +11,14 @@ export PYTHONPATH=$PYTHONPATH:$(pwd)
 
 split_index=$(($SLURM_ARRAY_TASK_ID))
 configs=(
-"python train.py --pooling 1 --bottleneck_dim 128 --size 'l' --dropout 0.2 --epochs 50 --cold_fasta 1 --gen_mol 1 --quantize 1"
-"python train.py --pooling 1 --bottleneck_dim 128 --size 'l' --dropout 0.2 --epochs 50 --cold_smiles 1 --quantize 1"
-"python train.py --pooling 1 --bottleneck_dim 128 --size 'l' --dropout 0.2 --epochs 50 --gen_mol 1 --quantize 1"
-"python train.py --pooling 1 --bottleneck_dim 128 --size 'l' --dropout 0.2 --epochs 50 --quantize 1"
-"python train.py --pooling 1 --bottleneck_dim 128 --size 'l' --dropout 0.2 --epochs 50 --cold_fasta 1 --gen_mol 1"
-"python train.py --pooling 1 --bottleneck_dim 128 --size 'l' --dropout 0.2 --epochs 50 --cold_smiles 1"
-"python train.py --pooling 1 --bottleneck_dim 128 --size 'l' --dropout 0.2 --epochs 50 --gen_mol 1"
-"python train.py --pooling 1 --bottleneck_dim 128 --size 'l' --dropout 0.2 --epochs 50"
+"python train.py --level $level --pooling 1 --bottleneck_dim 128 --size 'l' --dropout 0.2 --epochs 50 --cold_fasta 1 --gen_mol 1 --quantize 1 "
+"python train.py --level $level --pooling 1 --bottleneck_dim 128 --size 'l' --dropout 0.2 --epochs 50 --cold_smiles 1 --quantize 1"
+"python train.py --level $level --pooling 1 --bottleneck_dim 128 --size 'l' --dropout 0.2 --epochs 50 --gen_mol 1 --quantize 1"
+"python train.py --level $level --pooling 1 --bottleneck_dim 128 --size 'l' --dropout 0.2 --epochs 50 --quantize 1"
+"python train.py --level $level --pooling 1 --bottleneck_dim 128 --size 'l' --dropout 0.2 --epochs 50 --cold_fasta 1 --gen_mol 1"
+"python train.py --level $level --pooling 1 --bottleneck_dim 128 --size 'l' --dropout 0.2 --epochs 50 --cold_smiles 1"
+"python train.py --level $level --pooling 1 --bottleneck_dim 128 --size 'l' --dropout 0.2 --epochs 50 --gen_mol 1"
+"python train.py --level $level --pooling 1 --bottleneck_dim 128 --size 'l' --dropout 0.2 --epochs 50"
 )
 
 # Get the config for the current index
